8c37e19295b2e13803a58d4f1c6dcecbe41b91fa
Carl Evans <carl@webantic.co.uk>
2015-07-28 02:14:52 +0100

    Associated brand CPT with product CPT. Backend & admin pages nearly complete

--- woocommerce-brands.php
+++ woocommerce-brands.php
@@ -43,14 +43,62 @@ function deactivate_woocommerce_brands() {
  * The code that creates the custom post type
  * This action is documented in includes/woocommerce_brands-activator.php
  */
-function create_custom_post_type() {
+function wcb_create_custom_post_type() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/woocommerce_brands-activator.php';
 	Woocommerce_Brands_Activator::setup_post_type();
 }
 
-add_action( 'init', 'create_custom_post_type');
+add_action( 'init', 'wcb_create_custom_post_type');
 register_activation_hook( __FILE__, 'activate_woocommerce_brands' );
 register_deactivation_hook( __FILE__, 'deactivate_woocommerce_brands' );
+
+
+function wcb_add_custom_fields() {
+	global $woocommerce, $post;
+
+	  $output = '<div class="options_group">';
+		$args = array('post_type' => 'wcb_brand');
+		$the_query = null;
+		$the_query = new WP_Query($args);
+		if( $the_query->have_posts() ) {
+			$brand_ids = $brand_names = array();
+			foreach ($the_query->get_posts() as $key => $value) {
+				$brand_ids[] = $value->ID;
+				$brand_names[] = $value->post_title;
+			}
+			$select_options = array_combine($brand_ids, $brand_names);
+		}
+		wp_reset_query();  // Restore global post data stomped by the_post().
+		woocommerce_wp_select(
+			array(
+				'id'      => 'brand_select',
+				'label'   => 'Product\'s Brand',
+				'options' => $select_options
+				)
+			);
+	  // Custom fields will be created here...
+
+	  $output .= '</div>';
+	  echo $output;
+}
+add_action( 'woocommerce_product_options_general_product_data', 'wcb_add_custom_fields' );
+
+
+function wcb_save_custom_fields( $post_id ) {
+	$woocommerce_select = $_POST['brand_select'];
+	if( !empty( $woocommerce_select ) ) {
+		update_post_meta( $post_id, 'wcb_brand', esc_attr( $woocommerce_select ) );
+	}
+}
+add_action( 'woocommerce_process_product_meta', 'wcb_save_custom_fields' );
+
+
+
+
+
+
+
+
 
 /**
  * The core plugin class that is used to define internationalization,
